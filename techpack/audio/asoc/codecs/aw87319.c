21209483fdff3dadaa0f742685794e70b329eb19
Akmal <magchuz@pm.me>
2020-05-05 06:16:50 +0000

    codecs: aw87319: Remove unavailable header

--- techpack/audio/asoc/codecs/aw87319.c
+++ techpack/audio/asoc/codecs/aw87319.c
@@ -31,8 +31,6 @@
 #include <linux/moduleparam.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
-#include <linux/wakelock.h>
-
 
 #define AW87319_I2C_NAME	"AW87319_PA"
 #define AW87319_I2C_BUS	0
